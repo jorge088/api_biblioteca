74b2a3170affe39d2f8f14ef2277e780ee66b6f0
Jorge Mendez <mendezjorge98@gmail.com>
2023-08-11 02:25:15 -0300

    Class06 - activity

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,7 @@
 // middleware/errorHandler.js
 const errorHandler = (err, req, res, next) => {
     // Verificar si el error tiene un código de estado definido, de lo
-    //contrario, establecer el código de estado predeterminado
+    // contrario, establecer el código de estado predeterminado
     const statusCode = err.statusCode || 500;
     // Construir objeto de respuesta de error
     const errorResponse = {
